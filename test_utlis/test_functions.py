4aa61f496227057ab32936071a30a65d9f4da6cb
79096 <a.malakhinsky@gmail.com>
2024-03-12 18:22:38 +0300

    coverage 100% functions

--- test_utlis/test_functions.py
+++ test_utlis/test_functions.py
@@ -1,9 +1,9 @@
-import json
-import pytest
-from utlis.functions import import_json, find_operations_json, executed, filter_executed
+from utlis.functions import import_json, find_operations_json, executed, filter_executed, sorted_date
+
 
 def test_import_json():
     assert isinstance(import_json(), list) == True
+
 
 def test_find_operations_json():
     assert isinstance(find_operations_json(), str) == True
@@ -11,31 +11,18 @@ def test_find_operations_json():
     assert isinstance(find_operations_json(name_json='op'), str) == False
 
 
-
 def test_executed():
     assert executed({"state": "EXECUTED"}) == True
     assert not executed({"state": "PENDING"}) == True
 
 
-def test_filter_executed(monkeypatch):
-    mock_import_json = lambda: [
-        {"state": "EXECUTED", "date": "2023-01-01"},
-        {"state": "PENDING", "date": "2023-01-02"},
-        {"state": "EXECUTED", "date": "2023-01-03"}
-    ]
-    monkeypatch.setattr("your_module.import_json", mock_import_json)
-    assert list(filter_executed()) == [
-        {"state": "EXECUTED", "date": "2023-01-01"},
-        {"state": "EXECUTED", "date": "2023-01-03"}
-    ]
+def test_filter_executed():
+    assert ((list(filter_executed())[0])['state'] == "EXECUTED") == True
+    assert ((list(filter_executed())[0])['state'] == "Test") == False
 
-def test_sorted_date(monkeypatch):
-    mock_filter_executed = lambda: [
-        {"state": "EXECUTED", "date": "2023-01-01"},
-        {"state": "EXECUTED", "date": "2023-01-03"}
-    ]
-    monkeypatch.setattr("your_module.filter_executed", mock_filter_executed)
-    assert sorted_date() == [
-        {"state": "EXECUTED", "date": "2023-01-03"},
-        {"state": "EXECUTED", "date": "2023-01-01"}
-    ]
+    # filtered_data = [i for i in json_data if 'date' in i]
+
+
+def test_sorted_date():
+    assert isinstance(sorted_date(), list)
+    assert ('2019-12-08' in (sorted_date()[0])['date']) == True
