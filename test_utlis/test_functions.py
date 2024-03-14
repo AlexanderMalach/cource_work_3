9edd429734024bb6e37283f7230fc80cb3826f82
79096 <a.malakhinsky@gmail.com>
2024-03-14 19:23:15 +0300

    editing

--- test_utlis/test_functions.py
+++ test_utlis/test_functions.py
@@ -2,25 +2,25 @@ from utlis.functions import import_json, find_operations_json, executed, filter_
 
 
 def test_import_json():
-    assert isinstance(import_json(), list) == True
+    assert isinstance(import_json(), list) is True
 
 
 def test_find_operations_json():
-    assert isinstance(find_operations_json(), str) == True
-    assert isinstance(find_operations_json(name_json='op.json'), str) == False
-    assert isinstance(find_operations_json(name_json='op'), str) == False
+    assert isinstance(find_operations_json(), str) is True
+    assert isinstance(find_operations_json(name_json='op.json'), str) is False
+    assert isinstance(find_operations_json(name_json='op'), str) is False
 
 
 def test_executed():
-    assert executed({"state": "EXECUTED"}) == True
-    assert not executed({"state": "PENDING"}) == True
+    assert executed({"state": "EXECUTED"}) is True
+    assert not executed({"state": "PENDING"}) is True
 
 
 def test_filter_executed():
-    assert ((list(filter_executed())[0])['state'] == "EXECUTED") == True
-    assert ((list(filter_executed())[0])['state'] == "Test") == False
+    assert ((list(filter_executed())[0])['state'] == "EXECUTED") is True
+    assert ((list(filter_executed())[0])['state'] == "Test") is False
 
 
 def test_sorted_date():
     assert isinstance(sorted_date(), list)
-    assert ('2019-12-08' in (sorted_date()[0])['date']) == True
+    assert ('2019-12-08' in (sorted_date()[0])['date']) is True
